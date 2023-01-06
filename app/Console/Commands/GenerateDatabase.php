cbaf90f9f6e1574b50f4ae746c79c241b46c4370
Ahmed Ibrahim <42524100+Ahmed-Elesnawy@users.noreply.github.com>
2023-01-06 16:44:38 +0200

    Add Command To generate database

--- app/Console/Commands/GenerateDatabase.php
+++ app/Console/Commands/GenerateDatabase.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class GenerateDatabase extends Command
 {
@@ -11,7 +12,7 @@ class GenerateDatabase extends Command
      *
      * @var string
      */
-    protected $signature = 'db:generate';
+    protected $signature = 'db:generate {name?}';
 
     /**
      * The console command description.
@@ -27,6 +28,24 @@ class GenerateDatabase extends Command
      */
     public function handle()
     {
+        $schemaName = $this->argument('name') ?: config("database.connections.mysql.database");
+        $charset = config("database.connections.mysql.charset",'utf8mb4');
+        $collation = config("database.connections.mysql.collation",'utf8mb4_unicode_ci');
+
+        if (empty($schemaName)) {
+            $schemaName = $this->ask("Please specify a schema name ?");
+        }
+
+        config(["database.connections.mysql.database" => null]);
+
+        $query = "CREATE DATABASE IF NOT EXISTS $schemaName CHARACTER SET $charset COLLATE $collation;";
+
+        DB::statement($query);
+
+        config(["database.connections.mysql.database" => $schemaName]);
+
+        $this->info("Your database with name {$schemaName} generated successfully");
+
         return Command::SUCCESS;
     }
 }
